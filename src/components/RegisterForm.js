86ded62bb530837ea1da23c5267d06c638c80ac0
Juma <gerald.juma@student.moringaschool.com>
2025-06-26 22:21:08 +0300

    testing

--- src/components/RegisterForm.js
+++ src/components/RegisterForm.js
@@ -18,7 +18,7 @@ const RegisterForm = () => {
 
   const handleSubmit = async (values, { setSubmitting }) => {
     try {
-      const response = await fetch('http://localhost:5555/register', {
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/register`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(values),
