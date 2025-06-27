468f0cb45551908dd97443eb773fd8e976fe23a0
Juma <gerald.juma@student.moringaschool.com>
2025-06-27 11:52:57 +0300

    removed localhost fetch link and put the render one

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -17,13 +17,13 @@ const Profile = () => {
           navigate('/');
           return;
         }
-        const userResponse = await fetch('http://localhost:5555/me', {
+        const userResponse = await fetch('https://safe-space-group-project-backend-5.onrender.com/me', {
           headers: { Authorization: `Bearer ${token}` },
         });
         const userData = await userResponse.json();
         if (!userResponse.ok) throw new Error(userData.message || 'Failed to fetch profile');
 
-        const blogsResponse = await fetch(`http://localhost:5555/users/${userData.id}/blogs`);
+        const blogsResponse = await fetch(`https://safe-space-group-project-backend-5.onrender.com/users/${userData.id}/blogs`);
         const blogsData = await blogsResponse.json();
         if (!blogsResponse.ok) throw new Error('Failed to fetch blogs');
 
